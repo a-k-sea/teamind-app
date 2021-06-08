87f8688ed67c886c02d92e13cf2dcf00cb1563d9
Graciella Dharmawan <g.dharmawan@student.maastrichtuniversity.nl>
2021-06-08 12:41:49 +0200

    added validations & associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :memberships
+  has_many :user_answers
+  has_many :teams, through: :memberships
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
 end
