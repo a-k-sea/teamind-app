6e31c8130bdfb1b25d98e70decb2465fac8e1375
Graciella Dharmawan <g.dharmawan@student.maastrichtuniversity.nl>
2021-06-09 21:25:52 +0200

    questionnaire frontend & counter logic

---
+++ app/javascript/components/checkbox.js
@@ -0,0 +1,20 @@
+// JS to count checked checkboxes
+
+const countChecks = () => {
+  if (document.querySelector('#check-counter')) {
+    const checkboxes = document.querySelectorAll("input[type=checkbox]");
+    const max = 10
+    checkboxes.forEach((checkbox) => {
+      checkbox.addEventListener('change', function() {
+        var checkboxes_checked = document.querySelectorAll('input[type=checkbox]:checked').length;
+        if (checkboxes_checked > max) {
+          this.checked = false;
+        }
+        const counter = document.querySelector('#check-counter');
+        counter.innerHTML = `(${checkboxes_checked}/10)`
+      });
+    });
+  }
+};
+
+export { countChecks };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,8 +27,10 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { countChecks } from '../components/checkbox';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  countChecks();
 });
