101db85dee628787efd4869d229ac5d43593e6ff
Hellen van der Kroef <hellenvdkroef@gmail.com>
2021-06-09 15:05:38 +0200

    sessions controller in create_team using wizard next step helper

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # Wicked wizard
-  resources :create_team, only: :show
+  resources :create_team, only: [ :show, :update ]
   resources :teams, only: [ :index, :new, :create ]
 
   resources :questions, only: [:index, :new] do
