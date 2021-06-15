62f6e7ed6007a7db133a298ec0e1d653f4662e03
Hellen van der Kroef <hellenvdkroef@gmail.com>
2021-06-15 17:06:12 +0200

    optional true, removed

--- db/migrate/20210615075050_add_personalities_to_users.rb
+++ db/migrate/20210615075050_add_personalities_to_users.rb
@@ -1,6 +1,6 @@
 class AddPersonalitiesToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_reference :users, :personality, null: false, default: 17
+    add_reference :users, :personality, null: false
     add_foreign_key :users, :personalities
   end
 end
