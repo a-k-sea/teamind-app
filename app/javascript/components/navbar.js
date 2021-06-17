cff8d5f4ec6d2d083443bef0a4653f4dc1e390df
Ari Swedberg <akswedberg@gmail.com>
2021-06-17 11:09:19 +0200

    navbar becomes transparent white when scrolling

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
     const navbar = document.querySelector('.navbar-teamind');
     if (navbar) {
       window.addEventListener('scroll', () => {
-        if (window.scrollY >= window.innerHeight) {
+        if (window.scrollY >= 20) {
           navbar.classList.add('navbar-teamind-green');
         } else {
           navbar.classList.remove('navbar-teamind-green');
